ce83e4db04ffdbb29f30e53629298c6b4e35a6cb
Henrique <henriquegp17@gmail.com>
2025-06-03 23:50:36 -0300

    Atualizei o arquivo com as últimas mudanças

--- calCulos.java
+++ calCulos.java
@@ -1,14 +1,7 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package holerite;
 import java.util.Scanner;
-/**
- *
- * @author 051968
- */
+
 public class calCulos {
     Scanner entrada = new Scanner(System.in);
     String nome;
